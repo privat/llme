796721d763c6e05b103f158e1fa8b13216485637
Jean Privat <jean@pryen.org>
2025-10-21 17:29:55 -0400

    improve (anx fix) clitests.sh

--- tests/clitests.sh
+++ tests/clitests.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+# We just run things that should not fail (or not not fail)
+
 set -xe
 for config in *.toml; do
-	"$config" = "pyproject.toml" && continue
+	[ "$config" = "pyproject.toml" ] && continue
 
 	llme 'hello' 'world' -c "$config" -b >/dev/null
-	llme <<<'hello' 'world' -c "$config" -b >/dev/null
-	llme <<<'hello' -c "$config" -b >/dev/null
+	llme 'hello' 'world' -c "$config" -b --bulk >/dev/null
+	llme <<<'hello' 'world' -c "$config" >/dev/null
+	llme <<<'hello' -c "$config" >/dev/null
 	llme -v -v --config "$config" --dump-config > dumpconfig.txt
 	! llme -m bad hello
 	! llme -u bad hello
 	! llme --bad hello
 	! llme -c bad hello
 done
+
+echo "SUCCESS"
