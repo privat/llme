1254f0aae77dd39dbe32d9a9ff28a2860637b801
Your Name <your@email.com>
2025-10-29 18:10:19 -0400

    test: fix debug fib

--- tests/debug_fib.sh
+++ tests/debug_fib.sh
@@ -22,7 +22,7 @@ validate() {
 	fi
 }
 
-tllme 01 "The fib,c program prints 512 instead of 610. The assignment `a=b;`  id too late. Fix fit.c, compile it and run it." "$@" &&
+tllme 01 "The fib.c program prints 512 instead of 610. The assignment \`a=b;\` is too late. Fix fit.c, compile it and run it." "$@" &&
 validate
 
 tllme 02 "Compile and run fib.c" "Cat fib.c" "Spot the error" "Fix the error" "Compile it and run it again" "$@" &&
@@ -39,4 +39,3 @@ validate
 
 tllme 05 "Fix possible issues in ." "$@" &&
 validate
-
