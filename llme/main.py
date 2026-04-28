7aa72405537217086dc5543c7849596f88cb990d
Jean Privat <jean@pryen.org>
2026-04-28 12:10:08 -0400

    system_prompt: fix contextual info

--- llme/main.py
+++ llme/main.py
@@ -926,16 +926,16 @@ class LLME:
         system_prompt = self.config.system_prompt
         if self.config.tool_mode == "markdown":
             tool = all_tools["run_command"]
-            system_prompt += f"""## Tool run_command\n\nRun shell commands with a fenced code block and a `run` label. Format:\n\n```run $command\n$stdin\n```\n\nExample 1, list files:\n\n```run ls\n```\n\nExample 2, read file.txt:\n\n```run cat file.txt\n```\n\nExample 3, write "Hello" to file.txt\n\n```run cat > file.txt\nHello\n```\n\nExample 4, run a python script:\n\n```run python\nprint('Hello World')\n```\n\n"""
+            system_prompt += f"""\n## Tool run_command\n\nRun shell commands with a fenced code block and a `run` label. Format:\n\n```run $command\n$stdin\n```\n\nExample 1, list files:\n\n```run ls\n```\n\nExample 2, read file.txt:\n\n```run cat file.txt\n```\n\nExample 3, write "Hello" to file.txt\n\n```run cat > file.txt\nHello\n```\n\nExample 4, run a python script:\n\n```run python\nprint('Hello World')\n```\n\n"""
             system_prompt += tool.doc
-        date = self.direct_run_command("date").strip()
-        pwd = self.direct_run_command("pwd").strip()
 
         self.skills = skills.discover_skills(self.config.skills_path)
         if self.skills:
-            system_prompt += skills.prompt_for_skills(self.skills)
+            system_prompt += "\n" + skills.prompt_for_skills(self.skills)
 
-        system_prompt += f"date: {date}\npwd: {pwd}\n"
+        date = self.direct_run_command("date").decode().strip()
+        pwd = self.direct_run_command("pwd").decode().strip()
+        system_prompt += f"\n## Contextual information\n\ndate: {date}\npwd: {pwd}\n"
 
         return {"role": "system", "content": system_prompt}
 
